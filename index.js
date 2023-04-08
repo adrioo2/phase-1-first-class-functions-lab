359c157e5441608b6a042d4f54435f59f1aedf08
Lourdes Quizhpilema <adrianalou42@gmail.com>
2023-04-08 18:53:08 -0400

    pass

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    return drivers.slice(0,2) 
+}
+const returnLastTwoDrivers = function (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+    return drivers.slice(-2) 
+}
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers]
+function createFareMultiplier (fare) {
+    return function(multiplier){
+      return multiplier * fare
+    }
+  }
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+function selectDifferentDrivers(drivers, one){
+  return one(drivers)
+}
